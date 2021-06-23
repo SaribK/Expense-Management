47a765c9a76b4f6835da2cc85345914acae25ecb
SaribK <kashifsar01@gmail.com>
2021-06-22 21:42:18 -0400

    created form for reports

---
+++ Expense Management/frmReports.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Expense_Management
+{
+    public partial class frmReports : Form
+    {
+        public frmReports()
+        {
+            InitializeComponent();
+        }
+
+        // selects entries from the database that match a certain requirement and makes a table that can be copy pasted into excel, etc.
+    }
+}
