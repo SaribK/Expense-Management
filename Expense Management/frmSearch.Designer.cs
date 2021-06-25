f2f73c2f3e5a294cfb9d8a749316a315034bff56
SaribK <kashifsar01@gmail.com>
2021-06-25 16:49:56 -0400

    export button opens data in an excel sheet. Date option shows time, need to remove this part of the date in future.

--- Expense Management/frmSearch.Designer.cs
+++ Expense Management/frmSearch.Designer.cs
@@ -96,6 +96,8 @@ namespace Expense_Management
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(27, 302);
--- Expense Management/frmSearch.cs
+++ Expense Management/frmSearch.cs
@@ -128,34 +128,46 @@ namespace Expense_Management
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
-            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
-            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
-            worksheet = workbook.Sheets["Sheet1"];
-            worksheet = workbook.ActiveSheet;
-            worksheet.Name = "CustomerDetail";
-
-            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            if (dataGridView1.Rows.Count > 0)
             {
-                worksheet.Cells[i, 1] = dataGridView1.Columns[i - 1].HeaderText;
-            }
+                Microsoft.Office.Interop.Excel.Application xcelApp = new Microsoft.Office.Interop.Excel.Application();
+                xcelApp.Application.Workbooks.Add(Type.Missing);
 
-            for (int i = 0; i < dataGridView1.Rows.Count; i++)
-            {
-                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
                 {
-                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    xcelApp.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
                 }
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        xcelApp.Cells[i+2,j+1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+                }
+                xcelApp.Columns.AutoFit();
+                xcelApp.Visible = true;
             }
 
-            var saveFileDialogue = new SaveFileDialog();
-            saveFileDialogue.FileName = "output";
-            saveFileDialogue.DefaultExt = ".xlsx";
-            if (saveFileDialogue.ShowDialog() == DialogResult.OK)
-            {
-                workbook.SaveAs(saveFileDialogue.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-            }
-            app.Quit();
+
+
+            //This works
+            /*dataGridView1.SelectAll();
+            DataObject copydata = dataGridView1.GetClipboardContent();
+            if (copydata != null) Clipboard.SetDataObject(copydata);
+            Microsoft.Office.Interop.Excel.Application xlapp = new Microsoft.Office.Interop.Excel.Application();
+            xlapp.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook xlWbook;
+            Microsoft.Office.Interop.Excel.Worksheet xlsheet;
+            object misseddata = System.Reflection.Missing.Value;
+            xlWbook = xlapp.Workbooks.Add(misseddata);
+
+            xlsheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWbook.Worksheets.get_Item(1);
+            Microsoft.Office.Interop.Excel.Range xlr = (Microsoft.Office.Interop.Excel.Range)xlsheet.Cells[1, 1];
+            xlr.Select();
+
+            xlsheet.PasteSpecial(xlr, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+            */
         }
     }
 }
